cbc5cf735b09a64dcdd9556ba94143d031d33156
Brock Weekley <brockweekley@gmail.com>
2021-09-12 22:44:15 -0500

    Initialized python project

---
+++ uniformCostGraph.py
@@ -0,0 +1,24 @@
+import json
+
+if __name__ == '__main__':
+    print('Uniform Cost Graph')
+    inputFile = open('instanceOne.json')
+    squares = json.load(inputFile)['squares']
+    startingRow = 0
+    startingCol = 0
+
+    rowNum = 0
+    colNum = 0
+
+    for row in squares:
+        rowNum += 1
+        for col in row:
+            colNum += 1
+            if col == 2:
+                startingCol = colNum
+                startingRow = rowNum
+                squares[rowNum - 1][colNum - 1] = 0
+        colNum = 0
+
+    print(squares)
+    print(str(startingRow) + " " + str(startingCol))
